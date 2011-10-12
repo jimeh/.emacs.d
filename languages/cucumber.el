9880f0f77d1fc448e3d61af6f50a731c45b92cae
Jim Myhrberg <contact@jimeh.me>
2011-10-12 11:13:54 +0100

    fixes typo in cucumber init file

--- languages/cucumber.el
+++ languages/cucumber.el
@@ -1,6 +1,6 @@
 (add-to-list 'auto-mode-alist '("\\.feature" . feature-mode))
 
-(add-hook 'feature-mod-hook
+(add-hook 'feature-mode-hook
           (lambda ()
             (fci-mode)
             (setq whitespace-action (quote (auto-cleanup)))))
