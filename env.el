fc917547a5b7cd0843e3b244853ace830e13d344
Jim Myhrberg <contact@jimeh.me>
2011-11-09 13:37:58 +0000

    make emacs aware of my node.js bins directory
    This is a temporary hack pointed at the exact version of node.js I have installed. Hopefully I can come up with a more generic way to do it later.

--- env.el
+++ env.el
@@ -6,3 +6,7 @@
 (setenv "PATH" (concat "~/.rbenv/bin:" (getenv "PATH")))
 (setq exec-path (cons "~/.rbenv/shims" exec-path))
 (setenv "PATH" (concat "~/.rbenv/shims:" (getenv "PATH")))
+
+;; node.js
+(setq exec-path (cons "~/.nvm/v0.4.12/bin" exec-path))
+(setenv "PATH" (concat "~/.nvm/v0.4.12/bin:" (getenv "PATH")))
