40c1ab0b83df1f091abc4143cc92ca5d777066a1
Jim Myhrberg <contact@jimeh.me>
2011-11-12 00:04:11 +0000

    tried to get ECB working, but Cedet kept causing fatal errors

--- vendor.el
+++ vendor.el
@@ -1,2 +1,7 @@
 ;; Load-path
 (add-to-list 'load-path "~/.emacs.d/vendor")
+
+;; ECB -- Currently errors
+;; (load-file "~/.emacs.d/vendor/cedet-1.0/common/cedet.el")
+;; (add-to-list 'load-path "~/.emacs.d/vendor/ecb-2.40")
+;; (require 'ecb)
