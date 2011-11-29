119bf654d660f3a10303d9591d273e300da3e64b
Jim Myhrberg <contact@jimeh.me>
2011-11-29 23:16:07 +0000

    removed helper to execute ruby code, as ruby-compilation supplies functions for this

--- helpers.el
+++ helpers.el
@@ -1,23 +1,4 @@
 ;;
-;; Execute Ruby Code
-;;
-
-(defun is-rails-project ()
-  (when (textmate-project-root)
-    (file-exists-p (expand-file-name "config/environment.rb" (textmate-project-root)))))
-
-(defun run-rails-test-or-ruby-buffer ()
-  (interactive)
-  (if (is-rails-project)
-      (let* ((path (buffer-file-name))
-             (filename (file-name-nondirectory path))
-             (test-path (expand-file-name "test" (textmate-project-root)))
-             (command (list ruby-compilation-executable "-I" test-path path)))
-        (pop-to-buffer (ruby-compilation-do filename command)))
-    (ruby-compilation-this-buffer)))
-
-
-;;
 ;; Auto-indent on Paste
 ;;
 
