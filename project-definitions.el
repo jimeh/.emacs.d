ed4e631be4ac7b57cc6bead0a514faf5deb15e0c
Jim Myhrberg <contact@jimeh.me>
2011-12-07 09:55:04 +0000

    tweaked project definitions

--- project-definitions.el
+++ project-definitions.el
@@ -2,4 +2,13 @@
   (define-project-type ruby (generic)
     (or (look-for "Rakefile") (look-for "Gemfile") (look-for "config.ru")
         (look-for "\.gemspec$") (look-for "Capfile"))
-    :irrelevant-files ("vendor/bundle/" "coverage/" "^#.*#$" "\.DS_Store")))
+    :irrelevant-files ("^[#]" ".git/" "vendor/bundle/" "coverage/"
+                       "\.DS_Store"))
+
+  (define-project-type node-js (generic)
+    (look-for "package.json")
+    :irrelevant-files ("^[#]" ".git/" "node_modules"))
+
+  (define-project-type coffeescript (node-js)
+    (look-for "Cakefile"))
+)
