705f3a479d98a5ead8f22ea7a35140556352c2be
Jim Myhrberg <contact@jimeh.me>
2011-12-07 23:14:31 +0000

    ignore all of vendor in eproject

--- project-definitions.el
+++ project-definitions.el
@@ -2,7 +2,7 @@
   (define-project-type ruby (generic)
     (or (look-for "Rakefile") (look-for "Gemfile") (look-for "config.ru")
         (look-for "\.gemspec$") (look-for "Capfile"))
-    :irrelevant-files ("^[#]" ".git/" "vendor/bundle/" "coverage/"
+    :irrelevant-files ("^[#]" ".git/" "vendor/" "coverage/"
                        "\.DS_Store"))
 
   (define-project-type node-js (generic)
