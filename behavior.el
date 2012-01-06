14c38e38ffa04a31478fedae49bd70fa62cab962
Jim Myhrberg <contact@jimeh.me>
2012-01-06 09:23:59 +0000

    split out behavior stuff from init.el into behavior.el

---
+++ behavior.el
@@ -0,0 +1,13 @@
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
+
+(delete-selection-mode 1)
+
+;; (winner-mode 1)
+
+;; (desktop-save-mode 1)
--- init.el
+++ init.el
@@ -5,24 +5,11 @@
 (load-file "~/.emacs.d/helpers.el")
 (load-file "~/.emacs.d/languages.el")
 (load-file "~/.emacs.d/vendor.el")
+(load-file "~/.emacs.d/behavior.el")
 (load-file "~/.emacs.d/appearance.el")
 (load-file "~/.emacs.d/keybindings.el")
 (load-file "~/.emacs.d/remember.el")
 (load-file "~/.emacs.d/project-definitions.el")
-
-(setq-default tab-width 2)
-(setq-default indent-tabs-mode nil)
-
-(fset 'yes-or-no-p 'y-or-n-p)
-
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
-
-(delete-selection-mode 1)
-
-;; (winner-mode 1)
-
-;; (desktop-save-mode 1)
 
 ;; Custom variables file
 (setq custom-file "~/.emacs.d/custom-variables.el")
