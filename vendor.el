abe656d66dc12e8b7e48084239ca26dca84f2b02
Jim Myhrberg <contact@jimeh.me>
2012-01-06 09:26:54 +0000

    removed some legacy commented out stuff

--- vendor.el
+++ vendor.el
@@ -1,7 +1,2 @@
 ;; Load-path
 (add-to-list 'load-path "~/.emacs.d/vendor")
-
-;; ECB -- Currently errors
-;; (load-file "~/.emacs.d/vendor/cedet-1.0/common/cedet.el")
-;; (add-to-list 'load-path "~/.emacs.d/vendor/ecb-2.40")
-;; (require 'ecb)
