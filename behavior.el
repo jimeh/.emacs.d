998e6b2c5bc9cf4adbc9fb2bfe91a8e3eaa38218
Jim Myhrberg <contact@jimeh.me>
2012-01-07 20:27:00 +0000

    cleaned up behavior.el a bit

--- behavior.el
+++ behavior.el
@@ -1,16 +1,19 @@
+;; Default tab-width of 2 spaces
 (setq-default tab-width 2)
+
+;; Always indent with spaces
 (setq-default indent-tabs-mode nil)
 
+;; Answer yes/no questions by typing y/n
 (fset 'yes-or-no-p 'y-or-n-p)
 
+;; Enable C-x C-l and C-x C-u for down/up-case conversions
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 
+;; Globally enable delete-selection-mode
 (delete-selection-mode 1)
 
-;; (winner-mode 1)
-
-;; (desktop-save-mode 1)
 ;; scroll one line at a time (less "jumpy" than defaults)
 (setq mouse-wheel-scroll-amount '(2 ((shift) . 10)) ;; two lines at a time
       mouse-wheel-progressive-speed nil ;; don't accelerate scrolling
