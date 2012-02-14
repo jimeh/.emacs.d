fde75218cc7bac4e5626950a29d02b3a1f6aac3f
Jim Myhrberg <contact@jimeh.me>
2012-02-14 08:30:25 +0000

    don't need reb-re-syntax in custom-variables.el anymore

--- custom-variables.el
+++ custom-variables.el
@@ -69,7 +69,6 @@
 " "~/.org-files/notes.org" "Notes"))))
  '(org-reverse-note-order t)
  '(php-mode-dot-property-warning t)
- '(reb-re-syntax (quote string))
  '(ruby-deep-indent-paren nil)
  '(ruby-use-encoding-map nil)
  '(safe-local-variable-values (quote ((eval rainbow-mode 1))))
