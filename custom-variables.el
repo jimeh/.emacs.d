dc3831caa2d767b4f02bf96297dce498bcf0e2a9
Jim Myhrberg <contact@jimeh.me>
2012-03-04 22:23:20 +0000

    removed custom face for ecb as it's now included in the theme I use

--- custom-variables.el
+++ custom-variables.el
@@ -91,4 +91,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ecb-default-highlight-face ((t (:background "dark red")))))
+ )
