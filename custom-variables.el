1ace5d35a1841eb8228294ddc6f630fc0ff29abe
Jim Myhrberg <contact@jimeh.me>
2012-05-24 11:34:01 +0100

    Add/Remove a few ECB source paths

--- custom-variables.el
+++ custom-variables.el
@@ -24,7 +24,7 @@
  '(ecb-layout-name "left13")
  '(ecb-options-version "2.40")
  '(ecb-primary-secondary-mouse-buttons (quote mouse-1--mouse-2))
- '(ecb-source-path (quote (("~/Projects" "Projects") ("~/Projects/api" "api") ("~/.emacs.d" ".emacs.d") ("~/.dotfiles" ".dotfiles") ("~/src" "src") ("~/Projects/globaldev" "globaldev") ("~/Sites" "Sites") ("~" "~") ("/" "/"))))
+ '(ecb-source-path (quote (("~/Projects" "Projects") ("~/.emacs.d" ".emacs.d") ("~/.dotfiles" ".dotfiles") ("~/src" "src") ("~/Projects/fitzdares" "fitzdares") ("~/Sites" "Sites") ("~" "~") ("/" "/"))))
  '(ecb-tip-of-the-day nil)
  '(ecb-toggle-layout-sequence (quote ("left13" "left15")))
  '(ecb-tree-indent 3)
