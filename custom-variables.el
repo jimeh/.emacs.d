711e34ac88e6924282c0d6549486082c922290a6
Jim Myhrberg <contact@jimeh.me>
2013-06-02 15:32:36 +0300

    No idea why this changed when saving unrelated settings, but it did

--- custom-variables.el
+++ custom-variables.el
@@ -95,8 +95,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mumamo-background-chunk-major ((t nil)))
- '(mumamo-background-chunk-submode1 ((t nil)))
- '(mumamo-background-chunk-submode2 ((t nil)))
- '(mumamo-background-chunk-submode3 ((t nil)))
- '(mumamo-background-chunk-submode4 ((t nil))))
+ '(mumamo-background-chunk-major ((t nil)) t)
+ '(mumamo-background-chunk-submode1 ((t nil)) t)
+ '(mumamo-background-chunk-submode2 ((t nil)) t)
+ '(mumamo-background-chunk-submode3 ((t nil)) t)
+ '(mumamo-background-chunk-submode4 ((t nil)) t))
