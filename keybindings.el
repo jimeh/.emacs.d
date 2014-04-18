24ef61d9c0144840ad90fe03d1226ac4431e708b
Jim Myhrberg <contact@jimeh.me>
2014-04-18 16:05:40 +0100

    Tweak multiple-cursors keybindings

--- keybindings.el
+++ keybindings.el
@@ -7,7 +7,7 @@
 (global-set-key (kbd "C-c C-m") 'execute-extended-command)
 
 ;; Enable dabbrev-expand via custom keybinding.
-(global-set-key (kbd "C-c M-/") 'dabbrev-expand)
+(global-set-key (kbd "C-x M-/") 'dabbrev-expand)
 
 ;; If Smex is available, use it instead of extended-command
 (when (require 'smex nil 'noerror)
@@ -90,7 +90,7 @@
   (global-set-key (kbd "C-x C-SPC") 'mc/edit-lines) ;; GUI
   (global-set-key (kbd "M-/") 'mc/mark-next-like-this)
   (global-set-key (kbd "M-m") 'mc/mark-previous-like-this)
-  (global-set-key (kbd "C-c M-m") 'mc/mark-all-like-this)
+  (global-set-key (kbd "C-c M-/") 'mc/mark-all-like-this)
   (global-set-key (kbd "M-RET") 'set-rectangular-region-anchor)
   ;; Make alt-<click> add additional cursors
   (global-unset-key (kbd "M-<down-mouse-1>")) ;; must unset key first
