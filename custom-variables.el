2c7062d00b1f6817ea1893fb69dc6038c01a3463
Jim Myhrberg <contact@jimeh.me>
2014-05-13 23:21:37 +0100

    Increase scroll-margin from 3 to 4

--- custom-variables.el
+++ custom-variables.el
@@ -112,7 +112,7 @@
  '(projectile-globally-ignored-files (quote ("TAGS" "*.log")))
  '(ruby-use-encoding-map nil)
  '(safe-local-variable-values (quote ((eval rainbow-mode 1))))
- '(scroll-margin 3)
+ '(scroll-margin 4)
  '(scss-compile-at-save nil)
  '(search-exit-option nil)
  '(smooth-scroll-margin 6)
