38fa8a874a9b0caf41d1692be0d7dc4df4049ef6
Jim Myhrberg <contact@jimeh.me>
2014-05-23 23:27:19 +0100

    Disable keybindings used by other package

--- keybindings.el
+++ keybindings.el
@@ -163,8 +163,8 @@
 ;; Textmate-mode related keybindings
 (when (require 'textmate nil 'noerror)
   (global-set-key (kbd "C-t") 'textmate-goto-symbol)
-  (global-set-key (kbd "M-p") 'textmate-column-up)
-  (global-set-key (kbd "M-n") 'textmate-column-down)
+  ;; (global-set-key (kbd "M-p") 'textmate-column-up)
+  ;; (global-set-key (kbd "M-n") 'textmate-column-down)
   (global-set-key (kbd "C-c [") 'textmate-shift-left)
   (global-set-key (kbd "C-c ]") 'textmate-shift-right))
 
