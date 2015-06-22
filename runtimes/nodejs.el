3a057df7481324a4ba84e0bfebea4c71cb28cf16
Jim Myhrberg <contact@jimeh.me>
2015-06-22 22:44:28 +0100

    Update ruby and node.js environment setups

--- runtimes/nodejs.el
+++ runtimes/nodejs.el
@@ -2,8 +2,12 @@
 ;; Node.js setup
 ;;
 
-;; nvm
-(when (and
-       (file-directory-p (expand-file-name "~/.nvm"))
-       (require 'nvm nil 'noerror))
-  (nvm-use "v0.10.24"))
+;; node.js
+(setq exec-path (cons "~/.nvm/v0.12.4/bin" exec-path))
+(setenv "PATH" (concat "~/.nvm/v0.12.4/bin:" (getenv "PATH")))
+
+;; ;; nvm package - does not work >_<
+;; (when (require 'nvm nil 'noerror)
+;;  (setq nvm-dir (expand-file-name "~/.nvm/versions/node"))
+;;  (when (file-directory-p nvm-dir)
+;;    (nvm-use "v0.10.24")))
