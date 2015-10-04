44c74a389f527dc083a9dfc78f4e0860edc03cc0
Jim Myhrberg <contact@jimeh.me>
2015-10-04 08:52:36 +0100

    Clean slate, so I can add my rewritten config

--- init.el
+++ init.el
@@ -9,39 +9,3 @@
 ;; the `packages.el' file that's loaded in further down.
 ;;
 ;; (package-initialize)
-
-(setq emacs-config-dir
-      (file-name-directory (or load-file-name (buffer-file-name))))
-
-;; Determine if GUI or console settings should apply
-(setq gui-window-system
-      (if (or (getenv "EMACS_GUI_SERVER") window-system) t 'nil))
-
-;; Helper function for config path
-(defun get-config-path(path)
-  "Appends argument at the end of emacs-config-dir using expand-file-name"
-  (expand-file-name path emacs-config-dir))
-
-;; Load various config files
-(load-file (get-config-path "env.el"))
-(load-file (get-config-path "packages.el"))
-(load-file (get-config-path "helpers.el"))
-(load-file (get-config-path "mode-customizations.el"))
-(load-file (get-config-path "vendor.el"))
-(load-file (get-config-path "behavior.el"))
-(load-file (get-config-path "appearance.el"))
-(load-file (get-config-path "keybindings.el"))
-(load-file (get-config-path "aliases.el"))
-(load-file (get-config-path "remember.el"))
-(load-file (get-config-path "project-definitions.el"))
-
-;; Custom variables file
-(setq custom-file (get-config-path "custom-variables.el"))
-(load-file custom-file)
-
-;; Load runtime specific setup files
-(load-file (get-config-path "runtimes/ruby.el"))
-(load-file (get-config-path "runtimes/nodejs.el"))
-
-;; Initialize packages in packages.el
-(initialize-my-packages)
--- mode-customizations/ack-mode.el
+++
@@ -1,5 +0,0 @@
-(defun customizations-for-ack-mode ()
-  (interactive)
-  (define-key ack-mode-map (kbd "q") 'kill-this-buffer))
-
-(add-hook 'ack-mode-hook 'customizations-for-ack-mode)
