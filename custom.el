b5865cfc4d35ee2cefae719695ee628a6924b43f
Jim Myhrberg <contact@jimeh.me>
2016-01-31 10:44:07 +0000

    Customize face for perspective

--- custom.el
+++ custom.el
@@ -10,4 +10,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(persp-selected-face ((t (:foreground "#d97a35" :weight bold))))
  '(sp-pair-overlay-face ((t (:inherit nil)))))
