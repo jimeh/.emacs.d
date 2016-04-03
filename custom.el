4c2e455e86c832c4ff42eea05e27b639c5df9b46
Jim Myhrberg <contact@jimeh.me>
2016-04-03 10:29:32 +0100

    Emacs 25 pre-test stores list of installed packages

--- custom.el
+++ custom.el
@@ -5,6 +5,9 @@
  ;; If there is more than one, they won't work right.
  '(ecb-options-version "2.40")
  '(magit-commit-arguments (quote ("-S")))
+ '(package-selected-packages
+   (quote
+    (smart-shift move-dup ruby-refactor yasnippet yari yaml-mode workgroups2 web-mode undo-tree twilight-bright-theme twilight-anti-bright-theme thrift string-inflection smex smartparens smart-mode-line scss-mode sass-mode ruby-tools rubocop rainbow-mode rainbow-delimiters project-explorer phi-search persp-projectile neotree multiple-cursors markdown-mode magit lua-mode linum-relative json-mode js2-mode inf-ruby indent-guide ido-vertical-mode ido-ubiquitous highlight-symbol highlight-indentation helm-swoop helm-projectile helm-open-github grizzl gotest go-projectile gitignore-mode github-browse-file gitconfig-mode git-timemachine gist full-ack flycheck fill-column-indicator feature-mode expand-region exec-path-from-shell ecb easy-kill discover-my-major dired+ diminish diff-hl company-go coffee-mode buffer-move browse-kill-ring ace-window)))
  '(siren-rubocop-autocorrect-on-save nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
