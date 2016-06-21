5c63b959b611123de24dff4511c5b606fccf6402
Jim Myhrberg <contact@jimeh.me>
2016-06-21 21:09:05 +0100

    Switch from js2-mode to js-mode

--- modules/siren-js.el
+++ modules/siren-js.el
@@ -1,44 +1,44 @@
-;;; siren-js.el --- jimeh's Emacs Siren: js2-mode configuration.
+;;; siren-js.el --- jimeh's Emacs Siren: js-mode configuration.
 
 ;;; Commentary:
 
-;; Basic configuration for js2-mode.
+;; Basic configuration for js-mode.
 
 ;;; Code:
 
 (require 'siren-programming)
 
-(siren-require-packages '(js2-mode json-mode))
-(require 'js2-mode)
+(siren-require-packages '(jsfmt))
+(require 'jsfmt)
 
-(add-to-list 'auto-mode-alist '("\\.js\\'"    . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.pac\\'"   . js2-mode))
-(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'"    . js-mode))
+(add-to-list 'auto-mode-alist '("\\.pac\\'"   . js-mode))
+(add-to-list 'interpreter-mode-alist '("node" . js-mode))
 
-(eval-after-load 'js2-mode
-  '(progn
-     (defun siren-js2-mode-defaults ()
-       "Default tweaks for `js2-mode'."
-       (setq js2-basic-offset 2)
-       (setq tab-width 2)
-       (setq mode-name "JS2")
-       (js2-imenu-extras-mode +1)
+(defun siren-js-mode-defaults ()
+  "Default tweaks for `js-mode'."
+  (let ((indent-width 2))
+    (setq js-indent-level indent-width)
+    (setq indent-level indent-width)
+    (setq tab-width indent-width))
+  (setq jsfmt-command "standard-format")
 
-       (siren-prog-mode-defaults)
-       (company-mode +1)
-       (subword-mode +1)
-       (hs-minor-mode 1)
-       (highlight-indentation-current-column-mode)
-       (hideshowvis-enable)
+  (siren-prog-mode-defaults)
+  (company-mode +1)
+  (subword-mode +1)
+  (hs-minor-mode 1)
+  (highlight-indentation-current-column-mode)
+  (hideshowvis-enable)
 
-       (let ((map js2-mode-map))
-         (define-key map (kbd "C-j") 'newline-and-indent)
-         (define-key map (kbd "C-c C-h") 'siren-toggle-hiding)))
+  (let ((map js-mode-map))
+    (define-key map (kbd "C-j") 'newline-and-indent)
+    (define-key map (kbd "C-c C-h") 'siren-toggle-hiding)))
 
-     (setq siren-js2-mode-hook 'siren-js2-mode-defaults)
+(setq siren-js-mode-hook 'siren-js-mode-defaults)
 
-     (add-hook 'js2-mode-hook (lambda ()
-                               (run-hooks 'siren-js2-mode-hook)))))
+(add-hook 'js-mode-hook (lambda ()
+                          (run-hooks 'siren-js-mode-hook)))
+(add-hook 'before-save-hook 'jsfmt-before-save)
 
 (provide 'siren-js)
 ;;; siren-js.el ends here
