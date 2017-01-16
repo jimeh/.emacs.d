8f131a0acf78ebb02a1bb44bc446817804769ae8
Jim Myhrberg <contact@jimeh.me>
2017-01-16 12:34:57 +0000

    Add zone-choose function

--- modules/siren-zone.el
+++ modules/siren-zone.el
@@ -9,5 +9,15 @@
 (require 'zone)
 (zone-when-idle 120)
 
+(defun zone-choose (pgm)
+  "Choose a PGM to run for `zone'."
+  (interactive
+   (list
+    (completing-read
+     "Program: "
+     (mapcar 'symbol-name zone-programs))))
+  (let ((zone-programs (list (intern pgm))))
+    (zone)))
+
 (provide 'siren-zone)
 ;;; siren-zone.el ends here
