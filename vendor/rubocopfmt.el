4836b115060565a3393565a4cefbc9c89acd54f3
Jim Myhrberg <contact@jimeh.me>
2017-01-31 09:40:55 +0000

    Update rubocopfmt.el

--- vendor/rubocopfmt.el
+++ vendor/rubocopfmt.el
@@ -3,14 +3,34 @@
 ;; Copyright (C) 2017 Jim Myhrberg
 
 ;; Author: Jim Myhrberg
-;; Version: 0.1.0
 ;; Keywords: ruby rubocop rubocopfmt
-;; URL: https://github.com/jimeh/rubocopfmt.el
+;; URL: https://github.com/jimeh/rubocopfmt-emacs
+;; Version: 0.1.0
 
 ;; This file is not part of GNU Emacs.
 
-;;; Commentary:
+;;; License:
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining a
+;; copy of this software and associated documentation files (the "Software"),
+;; to deal in the Software without restriction, including without limitation
+;; the rights to use, copy, modify, merge, publish, distribute, sublicense,
+;; and/or sell copies of the Software, and to permit persons to whom the
+;; Software is furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+;; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+;; DEALINGS IN THE SOFTWARE.
 
+;;; Commentary:
+;;
 ;; The core parts of rubocopfmt.el are borrowed from the gofmt related parts of
 ;; go-mode.el 1.4.0. So most credit goes to The Go Authors.
 
@@ -33,6 +53,7 @@
         (coding-system-for-write 'utf-8)
         (rubocopfmt-args
          (list "--diff-format" "rcs"
+               "--interactive"
                "--src-dir" (file-name-directory (file-truename buffer-file-name)))))
 
     (unwind-protect
