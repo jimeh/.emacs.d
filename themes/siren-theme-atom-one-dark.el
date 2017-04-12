959aa1e0d5c9beb4c3c65631b9d73e7c6c764f6e
Jim Myhrberg <contact@jimeh.me>
2017-04-12 23:47:36 +0100

    Add theme definition for atom-one-dark

---
+++ themes/siren-theme-atom-one-dark.el
@@ -0,0 +1,14 @@
+;;; siren-theme-atom-one-dark.el --- jimeh's Emacs Siren: atom-one-dark-theme.
+
+;;; Commentary:
+
+;; Setup for atom-one-dark-theme.
+
+;;; Code:
+
+(siren-require-packages '(atom-one-dark-theme))
+
+(require 'atom-one-dark-theme)
+
+(provide 'siren-theme-atom-one-dark)
+;;; siren-theme-twilight-anti-bright.el ends here
