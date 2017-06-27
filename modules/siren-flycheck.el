ff30c8c049299f145bdc940c92ff34498ea2e465
Jim Myhrberg <contact@jimeh.me>
2017-06-27 15:25:17 +0100

    Update flycheck config

--- modules/siren-flycheck.el
+++ modules/siren-flycheck.el
@@ -13,6 +13,7 @@
 (setq flycheck-idle-change-delay 1.0)
 (setq flycheck-indication-mode 'right-fringe)
 (setq flycheck-ruby-rubocop-executable "rubocop-bundle-safe")
+(setq flycheck-javascript-standard-executable "semistandard")
 
 (provide 'siren-flycheck)
 ;;; siren-flycheck.el ends here
