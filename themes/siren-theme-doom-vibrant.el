71ed6bb822e1debef5ded66227add131b6c977ce
Jim Myhrberg <contact@jimeh.me>
2017-12-15 13:23:29 +0000

    Add theme module for doom-vibrant theme

---
+++ themes/siren-theme-doom-vibrant.el
@@ -0,0 +1,32 @@
+;;; siren-theme-doom-vibrant.el --- jimeh's Emacs Siren: doom-vibrant theme.
+
+;;; Commentary:
+
+;; Setup for doom-vibrant theme.
+
+;;; Code:
+
+(use-package doom-themes
+  :ensure t
+  :config
+  (require 'doom-themes)
+
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+  ;; Load the theme (doom-vibrant, doom-molokai, etc); keep in mind that each theme
+  ;; may have their own settings.
+  (load-theme 'doom-vibrant t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+
+  ;; Enable custom neotree theme
+  (doom-themes-neotree-config)  ; all-the-icons fonts must be installed!
+
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+(provide 'siren-theme-doom-vibrant)
+;;; siren-theme-doom-vibrant.el ends here
