f668167f31542bc2f6544af5a06476661f1f967c
Jim Myhrberg <contact@jimeh.me>
2017-12-17 14:06:54 +0000

    Update and tweak helm and helm-projectile config

--- modules/siren-helm-projectile.el
+++ modules/siren-helm-projectile.el
@@ -11,9 +11,15 @@
 (use-package helm-projectile
   :bind
   ("C-x ;" . helm-projectile)
-  ("C-x C-;" . helm-projectile)
+  ("C-x C-;" . helm-projectile-find-file)
   ("C-c ;" . helm-projectile-switch-project)
-  ("C-c C-;" . helm-projectile-switch-project))
+  ("C-c C-;" . helm-projectile-switch-project)
+
+  :config
+  ;; Don't bother with using files in open Dired buffer as initial source for
+  ;; helm-projectile-find-file, just use the list of files.
+  (setq helm-source-projectile-files-and-dired-list
+        '(helm-source-projectile-files-list)))
 
 (provide 'siren-helm-projectile)
 ;;; siren-helm-projectile.el ends here
--- modules/siren-helm.el
+++ modules/siren-helm.el
@@ -9,7 +9,32 @@
 (use-package helm
   :defer t
   :config
-  (setq helm-autoresize-mode 1))
+  (setq helm-autoresize-mode t
+        helm-autoresize-max-height 30
+        helm-autoresize-min-height 30
+        helm-buffer-max-length 64
+        helm-case-fold-search 'smart
+        helm-echo-input-in-header-line t
+        helm-file-name-case-fold-search 'smart
+        helm-split-window-default-side 'below)
+
+  ;; From: https://www.reddit.com/r/emacs/comments/3asbyn/new_and_very_useful_helm_feature_enter_search/
+  (defun helm-hide-minibuffer-maybe ()
+    (when (with-helm-buffer helm-echo-input-in-header-line)
+      (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+        (overlay-put ov 'window (selected-window))
+        (overlay-put ov 'face (let ((bg-color (face-background 'default nil)))
+                                `(:background ,bg-color :foreground ,bg-color)))
+        (setq-local cursor-type nil))))
+
+  ;; From: https://github.com/emacs-helm/helm/issues/918#issuecomment-81555133
+  (defun helm-toggle-header-line ()
+    (if (= (length helm-sources) 1)
+        (set-face-attribute 'helm-source-header nil :height 0.1)
+      (set-face-attribute 'helm-source-header nil :height 1.0)))
+
+  (add-hook 'helm-minibuffer-set-up-hook 'helm-hide-minibuffer-maybe)
+  (add-hook 'helm-before-initialize-hook 'helm-toggle-header-line))
 
 (use-package helm-descbinds
   :defer t)
