9cce78cbc978987670b1e564dd04d19178d79cc9
Jim Myhrberg <contact@jimeh.me>
2018-02-27 00:32:12 +0000

    Add `siren-helm--` prefix to custom helm functions

--- modules/siren-helm.el
+++ modules/siren-helm.el
@@ -19,7 +19,7 @@
         helm-split-window-default-side 'below)
 
   ;; From: https://www.reddit.com/r/emacs/comments/3asbyn/new_and_very_useful_helm_feature_enter_search/
-  (defun helm-hide-minibuffer-maybe ()
+  (defun siren-helm--hide-minibuffer-maybe ()
     (when (with-helm-buffer helm-echo-input-in-header-line)
       (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
         (overlay-put ov 'window (selected-window))
@@ -27,14 +27,15 @@
                                 `(:background ,bg-color :foreground ,bg-color)))
         (setq-local cursor-type nil))))
 
+  (add-hook 'helm-minibuffer-set-up-hook #'siren-helm--hide-minibuffer-maybe)
+
   ;; From: https://github.com/emacs-helm/helm/issues/918#issuecomment-81555133
-  (defun helm-toggle-header-line ()
+  (defun siren-helm--toggle-header-line ()
     (if (= (length helm-sources) 1)
         (set-face-attribute 'helm-source-header nil :height 0.1)
       (set-face-attribute 'helm-source-header nil :height 1.0)))
 
-  (add-hook 'helm-minibuffer-set-up-hook 'helm-hide-minibuffer-maybe)
-  (add-hook 'helm-before-initialize-hook 'helm-toggle-header-line))
+  (add-hook 'helm-before-initialize-hook #'siren-helm--toggle-header-line)
 
 (use-package helm-descbinds
   :defer t)
