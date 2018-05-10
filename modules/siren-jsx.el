e523eca5c0c05066b0898effc613bcf24f99fd8f
Jim Myhrberg <contact@jimeh.me>
2018-05-10 14:50:43 +0100

    Use rjsx-mode for ReactJS instead of web-mode

--- modules/siren-jsx.el
+++ modules/siren-jsx.el
@@ -6,8 +6,23 @@
 
 ;;; Code:
 
-(require 'siren-web-mode)
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(require 'siren-prettier-js)
+
+(use-package rjsx-mode
+  :mode "components\\/.*\\.js\\'"
+
+  :hook (rjsx-mode . siren-rjsx-mode-setup)
+
+  :init
+  (defun siren-rjsx-mode-setup ()
+    (siren-prog-mode-setup)
+
+    (prettier-js-mode)
+    (company-mode +1)
+    (subword-mode +1)
+    (hs-minor-mode 1)
+    (highlight-indentation-current-column-mode)
+    (hideshowvis-enable)))
 
 (provide 'siren-jsx)
 ;;; siren-jsx.el ends here
