928a34b7e2448985dc6429b4a88258acbcfb410b
Jim Myhrberg <contact@jimeh.me>
2019-03-05 00:22:42 +0000

    Use lsp-mode for rust

--- modules/languages/siren-rust.el
+++ modules/languages/siren-rust.el
@@ -8,6 +8,7 @@
 
 (require 'siren-folding)
 (require 'siren-highlight-indentation)
+(require 'siren-lsp)
 
 (use-package rust-mode
   :mode "\\.rs\\'"
@@ -23,6 +24,8 @@
   (defun siren-rust-mode-setup ()
     (setq rust-format-on-save t)
 
+    (company-mode +1)
+    (lsp-mode)
     (hs-minor-mode 1)
     (hideshowvis-enable)
     (highlight-indentation-current-column-mode)
@@ -34,21 +37,6 @@
 (use-package flycheck-rust
   :after rust-mode
   :hook (flycheck-mode . flycheck-rust-setup))
-
-(use-package racer
-  :requires rust-mode company
-  :commands racer-mode
-  :bind (:map rust-mode-map
-              ("TAB" . company-indent-or-complete-common))
-
-  :hook
-  (rust-mode . racer-mode)
-  (racer-mode . eldoc-mode)
-  (racer-mode . siren-racer-mode-setup)
-
-  :config
-  (defun siren-racer-mode-setup ()
-    (setq company-tooltip-align-annotations t)))
 
 (use-package rust-playground)
 
