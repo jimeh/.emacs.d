697da89a00d4e38fc3290e20c1fc8dd5c8ec8ad0
Jim Myhrberg <contact@jimeh.me>
2019-06-18 10:54:32 +0100

    Remove defunct custom-faces in custom.el
    These are now set via use-package's :custom-face option.

--- custom.el
+++ custom.el
@@ -12,6 +12,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(go-guru-hl-identifier-face ((t (:background "gray30"))))
- '(markdown-code-face ((t nil)))
- '(sp-pair-overlay-face ((t (:inherit nil)))))
+ )
