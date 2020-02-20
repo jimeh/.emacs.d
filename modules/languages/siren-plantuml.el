eb792ce1263ad5b6742244614e78ffa9cbfa15ca
Jim Myhrberg <contact@jimeh.me>
2020-02-20 23:30:55 +0000

    chore(lang): Improve plantuml-mode setup

--- modules/languages/siren-plantuml.el
+++ modules/languages/siren-plantuml.el
@@ -11,10 +11,12 @@
   :hook
   (plantuml-mode . siren-plantuml-mode-setup)
 
+  :custom
+  (plantuml-default-exec-mode 'executable)
+
   :init
   (defun siren-plantuml-mode-setup ()
-    (setq tab-width 2
-          plantuml-jar-path "/usr/local/opt/plantuml/libexec/plantuml.jar")))
+    (setq tab-width 2)))
 
 (provide 'siren-plantuml)
 ;;; siren-plantuml.el ends here
