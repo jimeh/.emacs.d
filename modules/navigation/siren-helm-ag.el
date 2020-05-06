34c130ae7e94be0608b34d71fcb6c99546ca4a0e
Jim Myhrberg <contact@jimeh.me>
2020-05-06 23:32:28 +0100

    chore(navigation): Use custom fork of helm-ag
    There's a few outstanding PRs and issues on helm-ag. I've collected the
    changes I care about into my own fork under a branch called
    `personal-patches`, and using that for now.

--- modules/navigation/siren-helm-ag.el
+++ modules/navigation/siren-helm-ag.el
@@ -9,12 +9,17 @@
 (require 'siren-helm)
 
 (use-package helm-ag
+  :straight (:type git :host github :repo "emacsorphanage/helm-ag"
+                  :fork (:host github :repo "jimeh/helm-ag"
+                               :branch "personal-patches"))
+
   :bind
   ("C-c C-s" . helm-do-ag)
   ("C-x C-'" . helm-do-ag-project-root)
 
   :custom
   (helm-ag-base-command "rg --no-heading")
+  (helm-ag-use-emacs-lisp-regexp nil)
   (helm-ag-ignore-patterns '("*.min-latest.css"
                              "*.min-latest.js"
                              "*.min.css"
