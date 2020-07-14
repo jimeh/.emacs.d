4338d85291bb48f247865796c9bfecf1eff1db09
Jim Myhrberg <contact@jimeh.me>
2020-07-14 02:27:21 +0100

    feat(shell): Switch shell-pop from ansi-term to vterm
    It performs much butter, and renders htop basically as fast as a normal
    terminal emulator.

--- modules/shell/siren-shell-pop.el
+++ modules/shell/siren-shell-pop.el
@@ -6,14 +6,16 @@
 
 ;;; Code:
 
+(require 'siren-vterm)
+
 (use-package shell-pop
   :bind
   ("C-`" . shell-pop)
 
   :custom
   (shell-pop-full-span t)
-  (shell-pop-shell-type '("ansi-term" "*ansi-term*"
-                          (lambda nil (ansi-term shell-pop-term-shell))))
+  (shell-pop-shell-type '("vterm" "*vterm*"
+                          (lambda nil (vterm shell-pop-term-shell))))
   (shell-pop-universal-key "C-`")
   (shell-pop-window-position "bottom")
   (shell-pop-window-size 40))
---
+++ modules/shell/siren-vterm.el
@@ -0,0 +1,14 @@
+;;; siren-vterm.el --- jimeh's Emacs Siren: vterm configuration.
+
+;;; Commentary:
+
+;; Basic configuration for vterm.
+
+;;; Code:
+
+(use-package vterm
+  :custom
+  (vterm-always-compile-module t))
+
+(provide 'siren-vterm)
+;;; siren-vterm.el ends here
