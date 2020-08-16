a8ec880b69dc5ce0d7a113d1a78054b2bd3bd7b2
Jim Myhrberg <contact@jimeh.me>
2020-08-16 11:35:44 +0100

    feat(shell): improve vterm-mode configuration
    - Enable switching windows with my custom M-i, M-k, M-j, and M-l
      windmove keybindings
    - Disable hl-line-mode for vterm buffers. The hl-line would constantly
      flicker in an annoying manner while typing.

--- modules/shell/siren-vterm.el
+++ modules/shell/siren-vterm.el
@@ -6,9 +6,25 @@
 
 ;;; Code:
 
+(require 'siren-windmove)
+
 (use-package vterm
+  :hook
+  (vterm-mode . siren-vterm-mode-setup)
+
+  :bind
+  (:map vterm-mode-map
+        ("M-i" . siren-windmove-up)
+        ("M-k" . siren-windmove-down)
+        ("M-j" . siren-windmove-left)
+        ("M-l" . siren-windmove-right))
+
   :custom
-  (vterm-always-compile-module t))
+  (vterm-always-compile-module t)
+
+  :init
+  (defun siren-vterm-mode-setup ()
+    (hl-line-mode -1)))
 
 (provide 'siren-vterm)
 ;;; siren-vterm.el ends here
