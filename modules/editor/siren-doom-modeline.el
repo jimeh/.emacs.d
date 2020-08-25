04b252438bec2ef4ef04e4808e2d21f813ae964b
Jim Myhrberg <contact@jimeh.me>
2020-08-25 01:41:50 +0100

    feat(workspaces): hide workspace name in modeline

--- modules/editor/siren-doom-modeline.el
+++ modules/editor/siren-doom-modeline.el
@@ -18,9 +18,11 @@
   (doom-modeline-checker-simple-format t)
   (doom-modeline-enable-word-count t)
   (doom-modeline-height 25)
-  (doom-modeline-persp-name nil)
+  (doom-modeline-indent-info nil)
   (doom-modeline-minor-modes t)
-  (doom-modeline-vcs-max-length 24))
+  (doom-modeline-persp-name nil)
+  (doom-modeline-vcs-max-length 24)
+  (doom-modeline-workspace-name nil))
 
 (provide 'siren-doom-modeline)
 ;;; siren-doom-modeline.el ends here
