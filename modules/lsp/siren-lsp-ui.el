3812b3df296e97f3cdd1303e73352c6802172534
Jim Myhrberg <contact@jimeh.me>
2020-11-15 17:11:51 +0000

    feat(lsp): bind lsp-ui-doc-glance to C-c C-d instead of lsp-ui-doc-show
    Also tweak lsp-ui-doc-* to show documentation at point

--- modules/lsp/siren-lsp-ui.el
+++ modules/lsp/siren-lsp-ui.el
@@ -11,13 +11,18 @@
 (use-package lsp-ui
   :defer t
   :bind (:map lsp-ui-mode-map
-              ("C-c C-d" . lsp-ui-doc-show)
+              ("C-c C-d" . lsp-ui-doc-glance)
               ("M-?" . lsp-ui-peek-find-references)
               ("C-c C-j" . lsp-ui-peek-find-definitions))
 
   :custom
   (lsp-ui-doc-enable nil)
-  (lsp-ui-sideline-enable nil))
+  (lsp-ui-sideline-enable nil)
+  (lsp-ui-doc-alignment 'window)
+  (lsp-ui-doc-delay 0)
+  (lsp-ui-doc-header nil)
+  (lsp-ui-doc-position 'at-point)
+  (lsp-ui-doc-use-webkit nil))
 
 (provide 'siren-lsp-ui)
 ;;; siren-lsp-ui.el ends here
