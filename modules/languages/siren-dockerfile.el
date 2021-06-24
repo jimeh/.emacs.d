79007f66879d59e8a3bbba0db36771492cefcb67
Jim Myhrberg <contact@jimeh.me>
2021-06-24 22:48:20 +0100

    chore(languages): remove temporary dockerfile-mode auto-mode-alist tweaks
    The PR that addressed the issue was merged long ago, and I simply forgot
    to remove the temporary fix :P

--- modules/languages/siren-dockerfile.el
+++ modules/languages/siren-dockerfile.el
@@ -10,22 +10,7 @@
   :hook
   (dockerfile-mode . siren-dockerfile-mode-setup)
 
-  ;; TODO: The auto-mode-alist customizations can be removed once this PR is
-  ;; merged: https://github.com/spotify/dockerfile-mode/pull/63
-  :mode
-  ;; Custom auto-mode-alist patterns requiring Dockerfiles to match of
-  ;; "Dockerfile", "Dockerfile.*", or "*.dockerfile".
-  "/Dockerfile\\(?:\\..*\\)?\\'"
-  "\\.dockerfile\\'"
-
   :init
-  ;; Delete default auto-mode-list from dockerfile-mode package, as it matches
-  ;; basically matches most filenames which contain "dockerfile", including this
-  ;; very file (siren-dockerfile.el).
-  (setq auto-mode-alist
-        (delete '("Dockerfile\\(?:\\..*\\)?\\'" . dockerfile-mode)
-                auto-mode-alist))
-
   (defun siren-dockerfile-mode-setup ()
     (subword-mode +1)))
 
