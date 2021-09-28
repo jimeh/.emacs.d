abc1dddcf968d5cad9ecb18eb0b94fecbea0694c
Jim Myhrberg <contact@jimeh.me>
2021-09-28 09:26:01 +0100

    fix(lang): set tab-width in protobuf-mode

--- modules/languages/siren-protobuf.el
+++ modules/languages/siren-protobuf.el
@@ -19,6 +19,7 @@
   :init
   (defun siren-protobuf-mode-setup ()
     (run-hooks 'prog-mode-hook)
+    (setq tab-width 2)
     (c-add-style "siren" '((c-basic-offset . 2)
                            (indent-tabs-mode . nil)) t)
 
