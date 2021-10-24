6daac868e22b4c08e3a5ab0d29a1ccc3196f72ef
Jim Myhrberg <contact@jimeh.me>
2021-10-24 03:56:51 +0100

    fix(lang/dockerfile): add custom hadolint flycheck checker which actually works

--- modules/languages/siren-dockerfile.el
+++ modules/languages/siren-dockerfile.el
@@ -12,7 +12,38 @@
 
   :init
   (defun siren-dockerfile-mode-setup ()
-    (subword-mode +1)))
+    (with-eval-after-load 'flycheck
+      (flycheck-select-checker 'hadolint))
+    (subword-mode +1))
+
+  (with-eval-after-load 'flycheck
+    ;; Define fixed Hadolint checker, built-in checker expects lines to start
+    ;; with "<filename>:", but when input is provided via STDIN, the each line
+    ;; starts with "-:".
+    (flycheck-define-checker hadolint
+      "A Dockerfile syntax checker using the hadolint.
+
+See URL `http://github.com/hadolint/hadolint/'."
+      :command ("hadolint" "--no-color" "-")
+      :standard-input t
+      :error-patterns
+      ((error line-start
+              "-:" line " " (id (one-or-more alnum)) " error: " (message)
+              line-end)
+       (warning line-start
+                "-:" line " " (id (one-or-more alnum))
+                " warning: " (message) line-end)
+       (info line-start
+             "-:" line " " (id (one-or-more alnum)) " info: " (message)
+             line-end)
+       (error line-start
+              "-:" line ":" column " " (message)
+              line-end))
+      :error-filter
+      (lambda (errors)
+        (flycheck-sanitize-errors
+         (flycheck-remove-error-file-names "/dev/stdin" errors)))
+      :modes dockerfile-mode)))
 
 (provide 'siren-dockerfile)
 ;;; siren-dockerfile.el ends here
