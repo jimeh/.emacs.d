36dd864a372f0eae3097c0064bbe8cce6036ef02
Jim Myhrberg <contact@jimeh.me>
2021-11-30 22:32:28 +0000

    chore(version-control): move forge SQLite database file to cache directory

--- modules/version-control/siren-forge.el
+++ modules/version-control/siren-forge.el
@@ -10,7 +10,11 @@
 
 (use-package forge
   :defer t
-  :after magit)
+  :after magit
+
+  :custom
+  (forge-database-file (expand-file-name
+                        "forge-database.sqlite" siren-cache-dir)))
 
 (use-package forge-post
   :straight forge
