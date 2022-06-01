13847908e2a9989a50b27bc9963570b3033e73d2
Jim Myhrberg <contact@jimeh.me>
2022-06-02 00:16:07 +0100

    chore(package): update straight.el and package lock file

--- core/siren-core-packages.el
+++ core/siren-core-packages.el
@@ -27,11 +27,11 @@
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el"
                          user-emacs-directory))
-      (bootstrap-version 5))
+      (bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
