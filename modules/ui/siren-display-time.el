e662ee6552a8b8ad0d076842a1a33c89195b0f59
Jim Myhrberg <contact@jimeh.me>
2022-07-30 22:20:55 +0100

    feat(ui/display-time): add config for display-time-mode

---
+++ modules/ui/siren-display-time.el
@@ -0,0 +1,23 @@
+;;; siren-display-time.el --- jimeh's Emacs Siren: display-time-mode.
+
+;;; Commentary:
+
+;; Basic configuration for display-time-mode.
+
+;;; Code:
+
+(use-package time
+  :straight (:type built-in)
+  :custom
+  (display-time-interval 1)
+  (display-time-24hr-format t)
+  (display-time-day-and-date t)
+  (display-time-load-average-threshold 0.0)
+  (display-time-default-load-average 0)
+  (display-time-format "%a %e %b %T")
+
+  :config
+  (display-time-mode t))
+
+(provide 'siren-display-time)
+;;; siren-display-time.el ends here
