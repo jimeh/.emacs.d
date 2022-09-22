f1a13a920e5a6838400ce6283f4a1a6ef50cb148
Jim Myhrberg <contact@jimeh.me>
2022-09-22 14:13:40 +0100

    chore(packages): override git-timemachine with fork that fixes bug

--- core/siren-core-package-overrides.el
+++ core/siren-core-package-overrides.el
@@ -8,6 +8,11 @@
 
 (setq straight-recipe-overrides
       '((nil . (
+                ;; Override go-playground to use my fork, which has a fix for
+                ;; uploading to go.dev/play.
+                (go-playground :type git :host github :repo "grafov/go-playground"
+                               :fork (:host github :repo "jimeh/go-playground"
+                                            :branch "fix-upload"))
                 ;; Override vertico recipe to fix issues with extensions.
                 (vertico :type git :host github :repo "minad/vertico"
                          :files (:defaults "extensions/*" (:exclude ".git")))))))
