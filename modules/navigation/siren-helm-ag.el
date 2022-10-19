6e9262da2cac31e22bedaf4b3f4c8bf5420c7a60
Jim Myhrberg <contact@jimeh.me>
2022-10-19 23:14:37 +0100

    fix(navigation/helm-ag): correctly set ignore patterns for ripgrep

--- modules/navigation/siren-helm-ag.el
+++ modules/navigation/siren-helm-ag.el
@@ -15,28 +15,30 @@
   ("C-x '" 'helm-do-ag-project-root)
   ("C-x C-'" 'helm-do-ag-project-root)
   (:keymaps 'helm-command-map
-        "a" 'helm-do-ag)
+            "a" 'helm-do-ag)
 
   :custom
-  (helm-ag-base-command (mapconcat 'identity
-                                   '("rg" "--no-heading" "--hidden"
-                                     "--glob !.git/*"
-                                     "--glob !.bundle/*"
-                                     "--glob !.vscode/*") " "))
-  (helm-ag-use-emacs-lisp-regexp nil)
-  (helm-ag-ignore-patterns '("*.min-latest.css"
-                             "*.min-latest.js"
-                             "*.min.css"
-                             "*.min.js"
-                             "*.sql"
-                             "*.test"
-                             "archive-contents"
-                             "cache"
-                             "elpa"
-                             "node_modules"
-                             "sorbet"
-                             "straight"
-                             "vendor/assets")))
+  (helm-ag-base-command "rg --no-heading --hidden")
+  (helm-ag-command-option (string-join '("--glob !*.cjs"
+                                         "--glob !*.log"
+                                         "--glob !*.min-latest.css"
+                                         "--glob !*.min-latest.js"
+                                         "--glob !*.min.css"
+                                         "--glob !*.min.js"
+                                         "--glob !*.mjs"
+                                         "--glob !*.sql"
+                                         "--glob !*.test"
+                                         "--glob !*/.log/*"
+                                         "--glob !*/.yarn/*"
+                                         "--glob !.bundle/*"
+                                         "--glob !.git/*"
+                                         "--glob !.log/*"
+                                         "--glob !.vscode/*"
+                                         "--glob !.yarn/*"
+                                         "--glob !node_modules/*"
+                                         "--glob !straight/*"
+                                         "--glob !vendor/*") " "))
+  (helm-ag-use-emacs-lisp-regexp nil))
 
 (provide 'siren-helm-ag)
 ;;; siren-helm-ag.el ends here
