5d1168337cdbf194ff267c2f2730bc21bd8401aa
Jim Myhrberg <contact@jimeh.me>
2023-05-09 01:33:49 +0100

    chore(ai): simplify chatgpt-shell setup as it is now in melpa

--- modules/ai/siren-chatgpt-shell.el
+++ modules/ai/siren-chatgpt-shell.el
@@ -9,13 +9,9 @@
 (require 'siren-chatgpt)
 
 (use-package shell-maker
-  :straight (:host github :repo "xenodium/chatgpt-shell"
-                   :files ("shell-maker.el"))
   :defer t)
 
 (use-package chatgpt-shell
-  :straight (:host github :repo "xenodium/chatgpt-shell"
-                   :files ("chatgpt-shell.el" "*-chatgpt-shell.el"))
   :defer t
 
   :custom
@@ -25,24 +21,14 @@
   :config
   ;; Set and manage API Key and Model via `siren-chatgpt' helpers.
   (siren-chatgpt-register-api-key-var 'chatgpt-shell-openai-key)
-  (siren-chatgpt-register-model-var 'chatgpt-shell-model-version)
-
-  ;; (require 'ob-chatgpt-shell)
-  ;; (ob-chatgpt-shell-setup)
-  )
+  (siren-chatgpt-register-model-var 'chatgpt-shell-model-version))
 
 (use-package dall-e-shell
-  :straight (:host github :repo "xenodium/chatgpt-shell"
-                   :files ("dall-e-shell.el" "*-dall-e-shell.el"))
   :defer t
 
   :config
   ;; Set and manage API Key and Model via `siren-chatgpt' helpers.
-  (siren-chatgpt-register-api-key-var 'dall-e-shell-openai-key)
-
-  ;; (require 'ob-dall-e-shell)
-  ;; (ob-dall-e-shell-setup)
-  )
+  (siren-chatgpt-register-api-key-var 'dall-e-shell-openai-key))
 
 (provide 'siren-chatgpt-shell)
 ;;; siren-chatgpt-shell.el ends here
