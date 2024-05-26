fe47210ec89c1e7a24ef33c41c9c3b8eed95e40a
Jim Myhrberg <contact@jimeh.me>
2024-05-26 23:57:03 +0100

    chore(language/toml): don't install conf-toml-mode if toml-ts-mode is available

--- modules/languages/siren-toml.el
+++ modules/languages/siren-toml.el
@@ -8,21 +8,21 @@
 
 (require 'siren-prog-mode)
 
-(use-package conf-toml-mode
-  :straight (:type built-in)
-  :mode "\\.toml\\'" "Cargo\\.lock\\'"
-  :hook (conf-toml-mode . siren-toml-mode-setup))
-
 ;; Requires Emacs 29.x or later for built-in treesit support.
-(when (fboundp 'toml-ts-mode)
-  (require 'siren-treesit)
+(if (fboundp 'toml-ts-mode)
   (use-package toml-ts-mode
     :straight (:type built-in)
     :mode "\\.toml\\'" "Cargo\\.lock\\'"
     :hook
     (toml-ts-mode . siren-toml-mode-setup)
+    :preface
+    (require 'siren-treesit)
     :config
-    (siren-treesit-auto-ensure-grammar 'toml)))
+    (siren-treesit-auto-ensure-grammar 'toml))
+  (use-package conf-toml-mode
+    :straight (:type built-in)
+    :mode "\\.toml\\'" "Cargo\\.lock\\'"
+    :hook (conf-toml-mode . siren-toml-mode-setup)))
 
 (defun siren-toml-mode-setup ()
   "Default tweaks for `toml-mode'."
