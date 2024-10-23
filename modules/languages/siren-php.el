c01da3c3b79e08646bde2ba810e03e2c40716838
Jim Myhrberg <contact@jimeh.me>
2024-10-24 00:38:09 +0100

    feat(language/php): switch to php-ts-mode

--- modules/languages/siren-php.el
+++ modules/languages/siren-php.el
@@ -6,6 +6,7 @@
 
 ;;; Code:
 
+(require 'siren-treesit)
 
 (use-package php-mode
   :interpreter "php"
@@ -15,6 +16,24 @@
 
   :preface
   (defun siren-php-mode-setup ()))
+
+(when (fboundp 'php-ts-mode)
+  (use-package php-ts-mode
+    :straight (:type built-in)
+    :interpreter "php"
+    :mode "\\.php\\'" "\\.inc\\'" "\\.module\\'"
+    :hook
+    (php-ts-mode . siren-php-ts-mode-setup)
+
+    :general
+    (:keymaps 'php-ts-mode-map
+              "C-j" 'newline-and-indent)
+
+    :custom
+    (php-ts-mode-indent-offset 2)
+
+    :preface
+    (defun siren-php-ts-mode-setup ())))
 
 (provide 'siren-php)
 ;;; siren-php.el ends here
