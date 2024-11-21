9313f84d51e581a9188683b3f90bb1a678c7e35d
Jim Myhrberg <jim@krystal.io>
2024-11-21 01:31:02 +0000

    fix(editor/origami): add missing major modes

--- modules/editor/siren-origami.el
+++ modules/editor/siren-origami.el
@@ -17,12 +17,16 @@
   :hook
   ((dart-mode
     go-mode
+    go-ts-mode
     js-mode
+    js-ts-mode
     json-mode
+    json-ts-mode
     php-mode
     protobuf-mode
     rjsx-mode
     rust-mode
+    rust-ts-mode
     typescript-mode) . siren-origami-mode-enable)
 
   :preface
@@ -32,10 +36,13 @@
   :config
   (dolist (item '((dart-mode . origami-c-style-parser)
                   (json-mode . origami-c-style-parser)
+                  (json-ts-mode . origami-c-style-parser)
                   (protobuf-mode . origami-c-style-parser)
                   (rjsx-mode . origami-c-style-parser)
                   (rust-mode . origami-c-style-parser)
-                  (typescript-mode . origami-c-style-parser)))
+                  (rust-ts-mode . origami-c-style-parser)
+                  (typescript-mode . origami-c-style-parser)
+                  (typescript-ts-mode . origami-c-style-parser)))
     (add-to-list 'origami-parser-alist item)))
 
 (use-package lsp-origami
